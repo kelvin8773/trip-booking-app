7273310d7c74e43302e91c356dddd830d27573c3
Kelvin Liang <kelvin9877@gmail.com>
2020-09-04 09:36:18 +0800

    adding header icons

--- src/navigation/tabs.js
+++ src/navigation/tabs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {Home} from '../screens';
